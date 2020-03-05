cebc38333bc5af9d4abdbe5cdcd34bc467c35938
viki-osherov <60446644+viki-osherov@users.noreply.github.com>
2020-03-05 15:16:59 -0600

    Create strings in python.py

---
+++ strings in python.py
@@ -0,0 +1,7 @@
+
+first_name = "nikol"
+last_name = "segal"
+full_name = f"{first_name} {last_name}"
+t = would you like to learn some Python today?
+message = f"Hello, {full_name.title(t)}!"
+print(message)
