94b15d1de81bc9df78281369aa42cbe492c86da0
viki-osherov <60446644+viki-osherov@users.noreply.github.com>
2020-03-06 15:07:06 -0600

    Create Organizing Lists.py

---
+++ Organizing Lists.py
@@ -0,0 +1,20 @@
+
+#  five places in the world that I want to visit. It's Rome, Israel, Paris, Russian, New York.
+
+locations = ['Rome', 'Israel', 'Paris', 'Russian', 'New York']
+print(locations)
+
+locations = ['Rome', 'Israel', 'Paris', 'Russian', 'New York']
+locations.sort(reverse=True)
+print(locations)
+
+locations = ['Rome', 'Israel', 'Paris', 'Russian', 'New York']
+locations.reverse()
+print(locations)
+
+locations = ['Rome', 'Israel', 'Paris', 'Russian', 'New York']
+locations.sort(reverse=True)
+print(locations)
+
+locations = ['Rome', 'Israel', 'Paris', 'Russian', 'New York']
+len(locations)
