5da4ca019302aebccd880d50d27826c884585591
viki-osherov <60446644+viki-osherov@users.noreply.github.com>
2020-03-25 12:41:01 -0600

    Create what_is_a_List.py

---
+++ what_is_a_List.py
@@ -0,0 +1,17 @@
+3-1.
+Names = ['Sylvin', 'gil', 'lilian', 'Jacqueline']
+print(Names[0])
+print(Names[1].title())
+print(Names[2].title())
+print(Names[3])
+
+3-3.
+car_mark = ['Ford', 'Audi', 'Peugeot', 'Toyota']
+zero = f"I would like to own a {car_mark[3].title()}."
+one = f"I would not like to ride a {car_mark[2].title()}."
+two = f"The first car that I remember is {car_mark[0].title()}."
+three = (f"{car_mark[1].title()}, is relay expensive car.")
+print(zero)
+print(one)
+print(two)
+print(three)
