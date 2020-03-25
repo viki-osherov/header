1e5cedbb7bc5ef85917556fdb790c797b3d662d5
viki-osherov <60446644+viki-osherov@users.noreply.github.com>
2020-03-25 15:16:41 -0600

    Create Removing_Elements.py

---
+++ Removing_Elements.py
@@ -0,0 +1,13 @@
+
+3-4.
+invite_to_dinner = [yon Tumarkin, anna Slu, mark Harmon]
+print(f"\nA {invite_to_dinner[0].title()}, please come to dinner in my home")
+print(f"\nA {invite_to_dinner[1].title()}, can you come to dinner, please?")
+print(f"\nA {invite_to_dinner[2].title()}, I invite you to dinner at my house")
+
+3-6.
+invite_to_dinner = [Yon_Tumarkin, Anna_Slu, Mark_Harmon]
+invite_to_dinner.insert(0, Joseph_Morgan)
+invite_to_dinner.insert(2, Katherine_McNamara)
+invite_to_dinner.append(Sarah_Michelle_Gellar)
+print(invite_to_dinner)
